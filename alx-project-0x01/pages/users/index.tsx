0c8773be2106f8a07b90d3813c7c7ed0d08cc364
Wilsad <wadehe@proton.me>
2025-08-06 23:22:20 +0000

    added maps

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,27 +1,41 @@
 import Header from "@/components/layout/Header";
 import UserCard from "@/components/common/UserCard";
-import { UserProps } from "@/interfaces";
+import UserModal from "@/components/common/UserModal";
+import { UserData, UserProps } from "@/interfaces";
+import { useState } from "react";
 
-interface UsersPageProps {
-  posts: UserProps[];
-}
+const Users: React.FC<{ posts: UserProps[] }> = ({ posts }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [user, setUser] = useState<UserData | null>(null);
 
-const Users: React.FC<UsersPageProps> = ({ posts }) => {
+  const handleAddUser = (newUser: UserData) => {
+    setUser({ ...newUser, id: posts.length + 1 });
+  };
+
   return (
     <div className="flex flex-col h-screen">
       <Header />
       <main className="p-4">
         <div className="flex justify-between">
           <h1 className="text-2xl font-semibold">User List</h1>
-          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add User</button>
+          <button onClick={() => setModalOpen(true)} className="bg-blue-700 px-4 py-2 rounded-full text-white">
+            Add User
+          </button>
         </div>
         <div className="grid grid-cols-3 gap-2">
-          {posts.map((user, index) => <UserCard key={index} {...user} />)}
+          {
+            posts.map((user, index) => (
+              <UserCard key={index} {...user} />
+            ))
+          }
         </div>
       </main>
+      {isModalOpen && (
+        <UserModal onClose={() => setModalOpen(false)} onSubmit={handleAddUser} />
+      )}
     </div>
-  )
-}
+  );
+};
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -29,7 +43,7 @@ export async function getStaticProps() {
 
   return {
     props: { posts }
-  }
+  };
 }
 
 export default Users;
